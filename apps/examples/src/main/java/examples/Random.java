d9c8ffff972b42910ad353430730bcb8cb83c3fc
Gerald Point <gerald.point@labri.fr>
2020-01-17 14:16:45 +0100

    chg: add a common random generator usable by all examples.

---
+++ apps/examples/src/main/java/examples/Random.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2008 - 2020, Arnaud Casteigts and the JBotSim contributors <contact@jbotsim.io>
+ *
+ *
+ * This file is part of JBotSim.
+ *
+ * JBotSim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JBotSim is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JBotSim.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package examples;
+
+public class Random {
+    private static java.util.Random prng = new java.util.Random();
+
+    public void setSeed(long seed) {
+        prng.setSeed(seed);
+    }
+
+    public double random() {
+        return nextDouble();
+    }
+
+    public double nextDouble() {
+        return prng.nextDouble();
+    }
+
+    public int nextInt(int bound) {
+        return prng.nextInt(bound);
+    }
+}
